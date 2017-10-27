c7c427edcbc01f06a0f2699b9e21cb42c4d350df
martinFrank <martin.frank.privat@googlemail.com>
2017-10-27 14:05:01 +0200

    changed project type to maven project

---
+++ maplib/src/test/java/de/frank/martin/maplib/FieldTest.java
@@ -0,0 +1,14 @@
+package de.frank.martin.maplib;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FieldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
